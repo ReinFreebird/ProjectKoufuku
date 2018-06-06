87726ac4051432b6d718372186d3def5e85513fa
Aat <teamarcanasoul@gmail.com>
2018-06-06 21:38:12 +0700

    Initial commit

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character{
+
+    private string firstName;
+    private string lastName;
+    private string stageName;
+
+
+    //Basic stats
+    private int currentStamina;
+    private int maxStamina;
+    private int popularity;
+
+
+    //Skill ( 0 to *)
+    private int vocal;
+    private int expression;
+    private int dance;
+
+    //
+
+    //work ( -50 to 50)
+    private int confidence; //negative = timid, positive = confident
+    private int ethic; //negative = lazy, positive = hard-working
+    private int social; //negative = asocial, positive= socialable
+    
+    
+
+    public Character(string first, string last)
+    {
+        firstName = first;
+        lastName= last;
+        maxStamina = 100;
+    }
+
+    public string StageName
+    {
+        get
+        {
+            return stageName;
+        }
+        set
+        {
+            stageName = value;
+        }
+    }
+    public int CurrentStamina
+    {
+        get
+        {
+            return currentStamina;
+        }
+
+        set
+        {
+            currentStamina = value;
+        }
+    }
+    public int MaxStamina
+    {
+        get
+        {
+            return maxStamina;
+        }
+
+        set
+        {
+            maxStamina = value;
+        }
+    }
+    public int Popularity
+    {
+        get
+        {
+            return popularity;
+        }
+        set
+        {
+            popularity = value;
+        }
+    }
+
+}
---
+++ Assets/Script/MainGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Character x = new Character("Miyuki","Fukuroune");
+        x.StageName = "MIYUKI";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
